a17023a3fd8671f55faddde7c6d20c98537e0bc8
AlfonsoVA <alfonsova@ciencias.unam.mx>
2016-11-03 19:14:05 -0600

    hola_mundo.c en implementado

---
+++ hola_mundo.c
@@ -0,0 +1,9 @@
+//incluimos la cabecera estandar de entrada/salida, es decir las funciones estandar de C
+#include <stdio.h>
+
+//metodo main, en este caso tipo void para no tener que devolver algo
+void main()
+{
+	//printf para imprimir line al ejecutar el programa
+	printf("¡Hola, mundo!");
+}
